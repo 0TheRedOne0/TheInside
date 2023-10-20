c160242f3a4f1fc62b16be7cbe4f7037ab9b0deb
0TheRedOne0 <d34d.m4n.4rt@gmail.com>
2023-10-20 12:30:31 -0500

    DoorInfrastructure

--- DEMOTheInsideV1.0/Assets/Scripts/CharacterInput.cs
+++ DEMOTheInsideV1.0/Assets/Scripts/CharacterInput.cs
@@ -4,11 +4,53 @@ using UnityEngine;
 
 public class CharacterInput : MonoBehaviour
 {
-    //variables
+    //Movement variables
     public CharacterController controller;
     public float speed = 16;
 
+    //Inside/Outside Varibles
+    public bool Inside = false;
+
+    private void Start()
+    {
+       
+    }
+
+
     void Update()
+    {
+        Movement();
+        AmbientC();
+    }
+
+
+    //Esta función es para que podamos cambiar entre el inside y el outside
+    /*private void OnTriggerEnter(Collider other)
+    {
+        
+    }*/
+
+   
+
+    void AmbientC()
+    {
+     if (Inside = false && Input.GetKeyDown(KeyCode.Tab))
+        {
+
+            Debug.Log(" Inside");
+            Inside = true;
+
+        }
+     else if(Inside = true && Input.GetKeyDown(KeyCode.Tab))
+        {
+            Debug.Log("Cambio de perspectiva al Outside");
+            Inside = false;
+        }
+
+    }
+
+    //Función de Movimiento
+    void Movement()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
