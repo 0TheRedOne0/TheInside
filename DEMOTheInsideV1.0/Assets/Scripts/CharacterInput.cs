fae782b6e9e26cd400b4b3fefdb28fa840aa8942
PapiPeje <148014132+PapiPeje@users.noreply.github.com>
2023-11-20 20:35:37 -0600

    Ya estan los shaders, falta setearlos
    ei

--- DEMOTheInsideV1.0/Assets/Scripts/CharacterInput.cs
+++ DEMOTheInsideV1.0/Assets/Scripts/CharacterInput.cs
@@ -202,6 +202,9 @@ public class CharacterInput : MonoBehaviour
 
 
             if (selection.CompareTag("C"))
+            { 
+            
+            }
         }
 
        
---
+++ PROYECTO DE COSAS UTILES/Assets/scripts/Spotlight/SpotlightBehavior.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO borrar execute always cuando ya funcione la herramienta
+[ExecuteInEditMode]
+
+public class SpotlightBehavior : MonoBehaviour
+{
+    //Class variables
+    private Light spotlightComponent;
+    private Material defaultMaterial;
+    [SerializeField] private Material spotlightMaterial;
+
+    [Header("Spotlight Attributes")]
+    [SerializeField] private float lightIntesity = 1.0f;
+    [SerializeField] private float emissionIntesity = 1.0f;
+    [SerializeField] private Color lightColor = new Color();
+    [SerializeField] private float spotlightRange= 100f;
+    [SerializeField] private float innerConeAngle = 25.0f;
+    [SerializeField] private float outerConeAngle = 85.0f;
+
+    private int spotlightColorID;
+    private int emissionIntensityID;
+
+    private void OnValidate()
+    {
+       
+        SpotlightSetup();
+    }
+
+    private void SpotlightSetup()
+    {
+        // convert shader references  to ID
+        spotlightColorID = Shader.PropertyToID("_SPOTLIGHT_COLOR");
+        emissionIntensityID = Shader.PropertyToID("_EMISSION_INTENSITY");
+
+        defaultMaterial = GetComponentInChildren<Renderer>().material;
+        defaultMaterial = spotlightMaterial;
+        spotlightComponent = GetComponentInChildren<Light>();
+        spotlightComponent.type = LightType.Spot;
+        spotlightMaterial.hideFlags = HideFlags.HideAndDontSave;
+        spotlightComponent.hideFlags = HideFlags.HideAndDontSave; //bloquea los componentes
+        spotlightComponent.intensity = lightIntesity;
+        spotlightComponent.innerSpotAngle = innerConeAngle;
+        spotlightComponent.spotAngle = outerConeAngle;
+        spotlightComponent.range = spotlightRange;
+
+        spotlightComponent.color = lightColor;
+        spotlightMaterial.SetColor(spotlightColorID, lightColor);
+        spotlightMaterial.SetFloat(emissionIntensityID, emissionIntesity);
+    }
+
+}
