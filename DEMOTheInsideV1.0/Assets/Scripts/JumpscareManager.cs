1f1794b6c91cb70ff918a4f7153430aa80f005c0
PapiPeje <148014132+PapiPeje@users.noreply.github.com>
2023-11-30 23:16:16 -0600

    jumpscar wip

---
+++ DEMOTheInsideV1.0/Assets/Scripts/JumpscareManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpscareManager : MonoBehaviour
+{
+    //variables
+    public GameObject CamJump;
+    public GameObject CamMain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
